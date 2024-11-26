c9abfaa354ae6a6e97d03d31909b1d1c7784a7ca
mrichardsJBOIT <14255080+mrichardsJBOIT@users.noreply.github.com>
2024-11-27 10:57:06 +1100

    initial push, I think

--- app/models/categorized_product.rb
+++ app/models/categorized_product.rb
@@ -1,4 +1,8 @@
 class CategorizedProduct < ApplicationRecord
+  # Associations
   belongs_to :product
   belongs_to :category
+
+  # Validations to prevent duplicate associations
+  validates :product_id, uniqueness: { scope: :category_id }
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,11 @@
 class Category < ApplicationRecord
+  # Associations
+  has_many :categorized_products, dependent: :destroy
+  has_many :products, through: :categorized_products
+
+  # Validations
+  validates :name, presence: true, uniqueness: true
+
+  # implement a nested category structure with the `ancestry` gem
+  # has_ancestry
 end
